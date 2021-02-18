54fce33c06d4bfb6eb35d588c59a60b5b0abd47c
yoyo-a11 <yoyol264@deltalearns.ca>
2021-02-18 09:20:29 -0800

    Update task3.py

--- task3.py
+++ task3.py
@@ -20,9 +20,9 @@ That is an even integer
 
 """
 
-number = float(input("float number."))
-
+number = float(input("please enter number"))
 while number / 2 != int(number / 2):
-    print("That is not an even integer.")
-    number = float(input("float number."))
-print("That is an even integer.")
+    print("That is not an even integer")
+    number = float(input("please enter number"))
+
+print("That is an even integer")
