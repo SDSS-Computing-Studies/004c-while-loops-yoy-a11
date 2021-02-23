516c6e1e8e973c0b954e90d69bb70685d8ebf9cf
yoyo-a11 <yoyol264@deltalearns.ca>
2021-02-22 16:49:49 -0800

    Update problem2.py

--- problem2.py
+++ problem2.py
@@ -16,16 +16,9 @@ Enter a number: 4
 4 8 12 16 20 24 28 32 36 40 44 48
 """
 
-import math
-import time
-
-counter = input("Please enter a numbrer")
-float(counter) = str(counter).strip()
-while True:
-    print(float(counter), end =" ", flush=True)
-    float(counter) = counter + counter
-    # time.sleep(x) will pause the program at this point for x seconds where x is a float 
-    time.sleep(0.1)
-    if counter > (float(counter)*12):
-       break
-print("===================")
+counter = input("Enter a numbrer: ")
+counter = int(str(counter).strip())
+i = 1
+while i <= 12:
+    print(int(counter * i), end =" ", flush=True)
+    i += 1
