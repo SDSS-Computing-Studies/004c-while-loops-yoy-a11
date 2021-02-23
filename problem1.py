befa123c6a956d0dce5801219efb3785075a7bc5
yoyo-a11 <yoyol264@deltalearns.ca>
2021-02-22 16:22:35 -0800

    Update problem1.py

--- problem1.py
+++ problem1.py
@@ -12,6 +12,7 @@ any more and the program will end.
 inputs:
 str username
 str password
+
 outputs:
 Access granted
 Access denied
@@ -19,8 +20,7 @@ Access denied
 i = 0
 username = ''
 password = ''
-#24 -> 26..30 -> 24 -> 26 .. 30 -> 24 -> 26 .. 30 -> 24 -> 26
-#i=0       i=1      i=1       i=2     i=3       i=4      i=4         
+      
 while username != 'admin' or password !='12345':
     if i == 3:
         break
